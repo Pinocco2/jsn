6024f24ed9560bade614924e4a0b295502b53efc
Стефан <bukotko2009@gmail.com>
2024-08-31 19:22:45 +0300

    commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const age = 15;
+console.log(age);
+const name = "Stefan";
+console.log(name)
+const isStudent = confirm("Ви є студентом?");
+console.log(isStudent);
+const myString = ("Не зупиняйся");
+console.log(myString);
+let myNumber = 8;
+console.log(myNumber + 10)
+const myNull = null;
+console.log(myNull);
+const userName = prompt("Введіть своє ім'я:");
+alert(`Привіт, ${userName}!`);
+const isConfirmed = confirm("Підтвердити дію?");
+if (isConfirmed) {
+  alert("Дякую за підтвердження!");
+} 
+else {
+  alert("Дію відмінено!");
+}
+alert("Увага! Дія є небезпечною. Будь ласка, підтвердіть дію.");
+const isConfirmedp = confirm("Підтвердити дію?");
+if (isConfirmedp) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
